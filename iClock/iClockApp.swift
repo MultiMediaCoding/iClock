7028b6351cdeabd8ba8e9992c8222b78e5c2939c
Hans Poreda <hansporeda@icloud.com>
2023-12-14 17:48:29 +0100

    Initial Commit

---
+++ iClock/iClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  iClockApp.swift
+//  iClock
+//
+//  Created by Hans Poreda on 14.12.23.
+//
+
+import SwiftUI
+
+@main
+struct iClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
